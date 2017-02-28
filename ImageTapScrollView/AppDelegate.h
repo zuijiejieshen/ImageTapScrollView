16a8675271366ccc12ad72cca783f0d70ee80b7b
Ellisa <18940304114@163.com>
2017-02-28 14:14:52 +0800

    init

---
+++ ImageTapScrollView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ImageTapScrollView
+//
+//  Created by 王辉 on 2017/2/28.
+//  Copyright © 2017年 王辉. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ImageTapScrollView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ImageTapScrollView
+//
+//  Created by 王辉 on 2017/2/28.
+//  Copyright © 2017年 王辉. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
